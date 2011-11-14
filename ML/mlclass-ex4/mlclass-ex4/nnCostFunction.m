d3015762fd9cc7bee01e2b235468b1f01498538a
Jon Evans <jon@plantwars.com>
2011-11-13 21:04:31 -0700

    Finish ML Assignment 4

--- ML/mlclass-ex4/mlclass-ex4/nnCostFunction.m
+++ ML/mlclass-ex4/mlclass-ex4/nnCostFunction.m
@@ -39,6 +39,37 @@ Theta2_grad = zeros(size(Theta2));
 %         cost function computation is correct by verifying the cost
 %         computed in ex4.m
 %
+
+% Take y and recode into vectors
+I = eye(num_labels);
+Y = zeros(m, num_labels);
+for i = 1:m,
+  Y(i, :) = I(y(i), :);
+end
+
+% Now let's do forward propagation
+
+% Add bias to X to get a1
+a1 = [ones(m, 1) X];
+
+% z2 = theta(1)*a(1)
+z2 = a1*Theta1';
+
+% a2 = g(z2)
+gz2 = sigmoid(z2);
+% plus bias
+a2 = [ones(size(z2, 1), 1) gz2];
+
+% z3 = theta(2) * a(2)
+z3 = a2*Theta2';
+
+% a3 = g(z3)
+a3 = h = sigmoid(z3);
+
+regularization_term = (lambda*(sum(sum(Theta1(:, 2:end).^2, 2)) + sum(sum(Theta2(:, 2:end).^2, 2)))) / (2 * m);
+
+J = sum(sum(-Y .* log(h) - (1 - Y) .* log(1 - h), 2))/m + regularization_term;
+
 % Part 2: Implement the backpropagation algorithm to compute the gradients
 %         Theta1_grad and Theta2_grad. You should return the partial derivatives of
 %         the cost function with respect to Theta1 and Theta2 in Theta1_grad and
@@ -54,6 +85,14 @@ Theta2_grad = zeros(size(Theta2));
 %               over the training examples if you are implementing it for the 
 %               first time.
 %
+
+delta2 = (a3 - Y)'*a2;
+delta1_start = ((a3 - Y) * Theta2) .* sigmoidGradient([ones(size(z2, 1), 1) z2]);
+delta1 = delta1_start(:, 2:end)' * a1;
+
+Theta2_grad = delta2 / m;
+Theta1_grad = delta1 / m;
+
 % Part 3: Implement regularization with the cost function and gradients.
 %
 %         Hint: You can implement this around the code for
@@ -62,10 +101,15 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
+% Don't regularize the first column - thus replacing first column with zeros
+theta_one_for_regularization = [zeros(size(Theta1, 1), 1) Theta1(:, 2:end)];
+theta_two_for_regularization = [zeros(size(Theta2, 1), 1) Theta2(:, 2:end)];
 
+regularization_term_1 = (lambda * theta_one_for_regularization)/m;
+regularization_term_2 = (lambda * theta_two_for_regularization)/m;
 
-
-
+Theta1_grad += regularization_term_1;
+Theta2_grad += regularization_term_2;
 
 
 
