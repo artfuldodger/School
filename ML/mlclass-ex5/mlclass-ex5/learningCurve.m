2efd93f0213c19e3b00c0ddbdbe716cddc95de9b
Jon Evans <jon@plantwars.com>
2011-11-20 22:49:34 -0700

    ML5: Finish

--- ML/mlclass-ex5/mlclass-ex5/learningCurve.m
+++ ML/mlclass-ex5/mlclass-ex5/learningCurve.m
@@ -56,7 +56,11 @@ error_val   = zeros(m, 1);
 % ---------------------- Sample Solution ----------------------
 
 
-
+for i = 1:m
+  [theta] = trainLinearReg(X(1:i, :), y(1:i), lambda);
+  [error_train(i), dont_care] = linearRegCostFunction(X(1:i, :), y(1:i), theta, 0);
+  [error_val(i), dont_care] = linearRegCostFunction(Xval, yval, theta, 0);
+end
 
 
 
--- ML/mlclass-ex5/mlclass-ex5/validationCurve.m
+++ ML/mlclass-ex5/mlclass-ex5/validationCurve.m
@@ -40,7 +40,12 @@ error_val = zeros(length(lambda_vec), 1);
 %
 
 
-
+for i = 1:length(lambda_vec),
+  lambda = lambda_vec(i)
+  [theta] = trainLinearReg(X, y, lambda);
+  [error_train(i), dont_care] = linearRegCostFunction(X, y, theta, 0);
+  [error_val(i), dont_care] = linearRegCostFunction(Xval, yval, theta, 0);
+end
 
 
 
