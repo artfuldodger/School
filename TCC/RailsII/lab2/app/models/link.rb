50f9a3159cd06fa7448800ef3e807808f7760432
Jon Evans <jon@plantwars.com>
2012-02-03 22:01:38 -0700

    Link to link show view with number of comments

--- TCC/RailsII/lab2/app/models/link.rb
+++ TCC/RailsII/lab2/app/models/link.rb
@@ -5,6 +5,7 @@ class Link < ActiveRecord::Base
   has_many :link_votes
   has_many :voted_for_by_users, through: :link_votes, source: :user
   belongs_to :user
+  has_many :comments, class_name: 'LinkComment', source: :link_comments
 
   def points
     link_votes.size
