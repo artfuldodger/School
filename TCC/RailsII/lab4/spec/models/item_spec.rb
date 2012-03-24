0f79355431ce7ba57efda56054368dbe3a2da500
Jon Evans <jon@plantwars.com>
2012-03-24 17:39:33 -0600

    Add autotest, model specs

--- TCC/RailsII/lab4/spec/models/item_spec.rb
+++ TCC/RailsII/lab4/spec/models/item_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe Item do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#quantity" do
+    it "defaults to 0" do
+      Item.new(:quantity => nil).quantity.should be_zero
+    end
+
+    it "returns the set quantity otherwise" do
+      Item.new(:quantity => 7).quantity.should == 7
+    end
+  end
 end
--- TCC/RailsII/lab4/spec/models/store_spec.rb
+++ TCC/RailsII/lab4/spec/models/store_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe Store do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#keyboard_shortcut" do
+    it "returns the first letter of the store's name" do
+      Store.new(:name => 'Liquor Store').keyboard_shortcut.should == 'L'
+    end
+  end
 end
