9fe8ef54753762dfb6d1904a14fce1441016f495
Jon Evans <jon@plantwars.com>
2012-03-25 13:15:35 -0600

    Add simulate click stuff

--- TCC/RailsII/lab4/app/assets/javascripts/keymaster.js
+++ TCC/RailsII/lab4/app/assets/javascripts/keymaster.js
@@ -182,3 +182,71 @@
   if(typeof module !== 'undefined') module.exports = key;
 
 })(this);
+
+// Adding to make cross-platform click events possible.
+// jquery's .click() doesn't seem to work unless there's a jquery event handler attached,
+// and I just need it to click the link and follow the href as usual.
+// 
+// Plain JS .click() works in Firefox, but not Safari. boo.
+//
+// Source: http://stackoverflow.com/questions/6157929/how-to-simulate-mouse-click-using-javascript/6158050#6158050
+   function simulate(element, eventName)
+    {
+        var options = extend(defaultOptions, arguments[2] || {});
+        var oEvent, eventType = null;
+
+        for (var name in eventMatchers)
+        {
+            if (eventMatchers[name].test(eventName)) { eventType = name; break; }
+        }
+
+        if (!eventType)
+            throw new SyntaxError('Only HTMLEvents and MouseEvents interfaces are supported');
+
+        if (document.createEvent)
+        {
+            oEvent = document.createEvent(eventType);
+            if (eventType == 'HTMLEvents')
+            {
+                oEvent.initEvent(eventName, options.bubbles, options.cancelable);
+            }
+            else
+            {
+                oEvent.initMouseEvent(eventName, options.bubbles, options.cancelable, document.defaultView,
+          options.button, options.pointerX, options.pointerY, options.pointerX, options.pointerY,
+          options.ctrlKey, options.altKey, options.shiftKey, options.metaKey, options.button, element);
+            }
+            element.dispatchEvent(oEvent);
+        }
+        else
+        {
+            options.clientX = options.pointerX;
+            options.clientY = options.pointerY;
+            var evt = document.createEventObject();
+            oEvent = extend(evt, options);
+            element.fireEvent('on' + eventName, oEvent);
+        }
+        return element;
+    }
+
+    function extend(destination, source) {
+        for (var property in source)
+          destination[property] = source[property];
+        return destination;
+    }
+
+var eventMatchers = {
+    'HTMLEvents': /^(?:load|unload|abort|error|select|change|submit|reset|focus|blur|resize|scroll)$/,
+    'MouseEvents': /^(?:click|dblclick|mouse(?:down|up|over|move|out))$/
+}
+var defaultOptions = {
+    pointerX: 0,
+    pointerY: 0,
+    button: 0,
+    ctrlKey: false,
+    altKey: false,
+    shiftKey: false,
+    metaKey: false,
+    bubbles: true,
+    cancelable: true
+}
